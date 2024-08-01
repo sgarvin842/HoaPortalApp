86d9861f832c6b8554d0bf809c1868e315f25d9b
Scott <garvins84@yahoo.com>
2024-08-01 22:05:58 +0800

    Added the base entities used for the app

---
+++ CleanArchitecture.Domain/Entities/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HoaPortalApp.Domain.Entities
+{
+    public class Document: HOAItem
+    {
+        public string Content { get; set; }
+        public string getDocumentContent()
+        {
+            return "";
+        }
+        public override string GetDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CleanArchitecture.Domain/Entities/HOAItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HoaPortalApp.Domain.Entities
+{
+    public abstract class HOAItem
+    {
+        public string ItemId { get; set; }
+        public string Title { get; set; }
+        public Date CreatedDate { get; set; }
+
+        public abstract string GetDetails();
+    }
+}
