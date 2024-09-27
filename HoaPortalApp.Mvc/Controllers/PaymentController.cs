7e589fe08657227cc33ae9bf85809b7ad2ef4eb6
Scott <garvins84@yahoo.com>
2024-09-27 21:44:35 +0800

    Added scaffolding for all pages

---
+++ HoaPortalApp.Mvc/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+using HoaPortalApp.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HoaPortalApp.Mvc.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Payments()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- HoaPortalApp.Mvc/Program.cs
+++ HoaPortalApp.Mvc/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
@@ -19,5 +19,29 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "Payments",
+    pattern: "{controller=Home}/{action=Payments}/{id?}");
+
+app.MapControllerRoute(
+    name: "ContactInfo",
+    pattern: "{controller=Home}/{action=ContactInfo}/{id?}");
+
+app.MapControllerRoute(
+    name: "MyItems",
+    pattern: "{controller=Home}/{action=MyItems}/{id?}");
+
+app.MapControllerRoute(
+    name: "CalendarAndEvents",
+    pattern: "{controller=Home}/{action=MyItems}/{id?}");
+
+app.MapControllerRoute(
+    name: "MyItems",
+    pattern: "{controller=Home}/{action=Directory}/{id?}");
+
+app.MapControllerRoute(
+    name: "MyItems",
+    pattern: "{controller=Home}/{action=Documents}/{id?}");
 
 app.Run();
